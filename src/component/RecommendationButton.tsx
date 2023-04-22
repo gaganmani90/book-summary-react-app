04149ab211606b351c0661353fe1d4e5054678be
Gagan Mani <gaganmani90@gmail.com>
2023-04-21 21:07:08 -0700

    initial commit

---
+++ src/component/RecommendationButton.tsx
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+function RecommendationButton() {
+    return (
+        <>
+            <Button variant="primary">Recommend 5 Books</Button>{' '}
+        </>
+    );
+}
+
+export default RecommendationButton;
